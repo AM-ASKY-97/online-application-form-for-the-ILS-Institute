920b3a12517e2df8cb6d7672361cd0cdfcf9b9a7
97PHPDeveloper <am.asky97@gmail.com>
2021-10-25 16:59:12 +0530

    form

--- index.php
+++ index.php
@@ -1,30 +1,164 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
-    <form action="form.php" method="post">
-        <label for="">Name : </label>
-        <input type="text" name="name">
-        <br><br>
+    <!--Css link-->
+    <link rel="stylesheet" href="style.css">
+</head>
+  <body>
+      
+     <div class="container">
+         <div class="row mt-3 ">
+              <!--logo-->
+              <div class="col-md-2">
+                  <img src="Najeem HETC.png" alt="" width="220px" height="270px" >
+              </div>
 
-        <label for="">Education : </label>
-        <label for="edu">GCE(O/L)</label>
-        <input type="checkbox" name="edu" id="edu">
-        <label for="ed">GCE(A/L)</label>
-        <input type="checkbox" name="edu" id="ed">
+              <!--Application-->
+              <div class="col-md-8 text-center">
+                  <h1 class="display-5">APPLICATION FORM</h1>
 
-        <br><br>
-        <label for="">Contact </label>
-        <input type="text" name="phone">
+                  <h4>S.L. Najeem (SLDS)</h4>
+                  <p> <i>Trainer of English 
+                    <br> MBA (Eng.Medium).B.A in Soc. Sci (Eng-Medium). HND in English 
+                    <br> DSW (Merit). DHRM, DMM, DBM 
+                    <br> Institute of Language Studies (ILS) 
+                    <br> Sammanthurai. </i> </p>
 
-        <br><br>
-        <input type="submit" value="Submit">
-    </form>
-</body>
+                  <h5><b>Mobile & Whatsapp : 077-6542915, 076-0104094</b></h5>  
+              </div>
+
+              <!--Photo-->
+              <div class="col-md-2">
+                  <div class="photo">Affix here a Passport Size Photo</div>
+              </div>
+        
+        </div>
+
+
+        <form action="">
+
+            <div class="form-qroup">
+                <div class="row mt-3 font-weight-bold">
+                    <div class="col-md-4">
+                        <label for="Course" class="">Admission To the Course of Study</label>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="DIPLOMA IN SPOKEN ENGLISH" id="english" >
+                                    <label class="form-check-label
+                                    " for="english">
+                                        DIPLOMA IN SPOKEN ENGLISH
+                                    </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="DIPLOMA IN SPOKEN ENGLISH" id="sinhala">
+                                        <label class="form-check-label" for="sinhala">
+                                        DIPLOMA IN SPOKEN SINHALA
+                                        </label>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>                
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-4 font-weight-bold">
+                        <label class="col-form-label" for="initial">1. Name with Initials</label>
+                    </div>
+
+                    <div class="col-md-8"> 
+                        <input type="text" class="form-control" id="initial" placeholder="Name with Initials">
+                    </div>
+                </div> 
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-4 font-weight-bold">
+                        <label class="col-form-label" for="Name">2. Name/Names denoted by Initials</label>
+                    </div>
+
+                    <div class="col-md-8">
+                        <input type="text" class="form-control" id="Name" placeholder="Name/Names denoted by Initials">
+                    </div>
+                </div> 
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-4 font-weight-bold">
+                        <label class="col-form-label" for="address">3. Address</label>
+                    </div>
+
+                    <div class="col-md-8">
+                        <input type="text" class="form-control" id="address" placeholder="Address">
+                    </div>
+                </div> 
+            </div>
+
+
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-4 font-weight-bold">
+                        <label class="col-form-label" for="address">3. Date of Birth</label>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div class="col-4">
+                                <input type="text" class="form-control"  placeholder="Year">
+                            </div>
+
+                            <div class="col-4">
+                                <input type="text" class="form-control"  placeholder="Month">
+                            </div>
+
+                            <div class="col-4">
+                                <input type="text" class="form-control"  placeholder="Day">
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div> 
+            </div>
+
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-4 font-weight-bold">
+                        <label class="col-form-label" for="nic">3. National Identity Card No</label>
+                    </div>
+
+                    <div class="col-md-8">
+                        <input type="text" class="form-control" id="nic" placeholder="National Identity Card No">
+                    </div>
+                </div> 
+            </div>
+
+        </form>
+     </div>   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
