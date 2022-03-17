a0b0f73958d5bb299a2fae942c3e4e5ffce8b112
AM-ASKY <am.asky97@gmail.com>
2022-03-17 21:27:02 +0530

    Portfolio URL changed

--- index.php
+++ index.php
@@ -330,7 +330,7 @@
 	        <div class="row text-center">
                 <div class="col bg-dark text-light text-uppercase pt-3">
                     <h6>&#169 Copyright 2021 ILS Institute. All right reserved. </h6> 
-                    <p>| Designed By : <a href="https://am-asky-resume.herokuapp.com/"> A.M. Asky</a> </p>
+                    <p>| Designed By : <a href="http://am-asky97.herokuapp.com/"> A.M. Asky</a> </p>
                 </div>
             </div>
         <!-- End Footer -->
